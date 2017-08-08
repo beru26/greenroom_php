85f6b0f29776c703955cc05253c70712edd0da87
beru26 <berkes.edina@gmail.com>
2017-08-08 11:16:40 +0200

    created table with random generated data

--- install.php
+++ install.php
@@ -1,20 +1,74 @@
-<?php
-    $dbhost = 'localhost:3036';
-    $dbuser = 'root';
-    $dbpass = 'rootpassword';
-    $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Greenroom</title>
+    </head>
+    <body>
 
-    if(! $conn) {
-        die('Connection failed! '.mysql_error());
-    }
-    echo 'Connection successfull';
+        <table>
+            <tr>
 
-    $sql = 'CREATE Database tractors_db';
-    $retval = mysql_db_query( $sql, $conn );
+            </tr>
+        </table>
+        <?php
+            $dbhost = '127.0.0.1';
+            $dbuser = 'root';
+            $dbpass = '';
+            $conn = mysql_connect($dbhost, $dbuser, $dbpass);
 
-    if(! $retval) {
-        die('Could not create database: ' .mysql_error());
-    }
-    echo 'Database created successfully.';
-    mysql_close($conn);
-?>
+            if(! $conn) {
+                die('Connection failed! '.mysql_error());
+            }
+            echo 'Connection successfull';
+
+            // Random generált adatok
+            function randomBrand{
+                $brand_list = array("Toyota", "Lada", "Mazda", "Suzuki");
+                return $brand_list[array_rand($brand_list)];
+            }
+
+            function randomType{
+                $brand_list = array("HUN", "EN", "CH", "GER");
+                return $type_list[array_rand($type_list)];
+            }
+
+            function randomPrice{
+                $brand_list = array(50, 80, 100, 120);
+                return $price_list[array_rand($price_list)];
+            }
+
+            function randomPerf{
+                $brand_list = array("60kW", "80kW", "90kW");
+                return $performance_list[array_rand($performance_list)];
+            }
+
+            function randomDescr{
+                $brand_list = array("new", "used", "mildly used");
+                return $description_list[array_rand($description_list)];
+            }
+
+            //Adatbázis létrehozása
+            $mysql_query = 'CREATE Database greenroom';
+            $db = mysql_select_db('greenroom');
+
+            //tractors tábla létrehozása
+            mysql_query = 'CREATE TABLE tractors(
+                brand VARCHAR(25) NOT NULL,
+                type VARCHAR(25) NOT NULL,
+                price INT NOT NULL,
+                performance TEXT NOT NULL,
+                description TEXT NOT NULL
+            )';
+            echo 'Table Created';
+
+            //Táblázat feltöltése 50 random generált traktorral
+            $counter = 0;
+            while($counter <= 50){
+                $mysql_query("INSERT INTO tractors VALUES(
+                    randomBrand(),randomType(),randomPrice(),
+                    randomPerf(),randomDescr())";
+            }
+        ?>
+    </body>
+</html>
