db79687b6fcf5ff9ddd9f098d7a9fdbcc91e7c23
beru26 <berkes.edina@gmail.com>
2017-08-13 20:48:00 +0200

    import the config file

--- install.php
+++ install.php
@@ -1,88 +1,74 @@
-<?php
+<?php include 'config.php';
 
-/* Connect to a MySQL database using driver invocation */
-$dsn = 'mysql:dbname=greenroom;host=127.0.0.1';
-$user = 'root';
-$password = 'podberbog';
+//create tractors table
+$mysql_query = "CREATE TABLE IF NOT EXISTS tractors(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    brand VARCHAR(25) NOT NULL,
+    type VARCHAR(25) NOT NULL,
+    price INT NOT NULL,
+    performance TEXT NOT NULL,
+    description TEXT NOT NULL
+)";
 
-try {
-    $conn = new PDO($dsn, $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    //create tractors table
-    $mysql_query = "CREATE TABLE IF NOT EXISTS tractors(
-        id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-        brand VARCHAR(25) NOT NULL,
-        type VARCHAR(25) NOT NULL,
-        price INT NOT NULL,
-        performance TEXT NOT NULL,
-        description TEXT NOT NULL
-    )";
-
-    $stmt = $conn->query($mysql_query);
-    if ($stmt) {
-        echo 'Table Created<br>';
-    } else {
-        echo 'Table creation failed';
-    }
-
-    //Random genereated data
-
-    function randomBrand(){
-        $brand_list = array("Toyota", "Lada", "Mazda", "Suzuki");
-        return $brand_list[array_rand($brand_list)];
-    }
-
-    function randomType(){
-        $type_list = array("HUN", "EN", "CH", "GER");
-        return $type_list[array_rand($type_list)];
-    }
-
-    function randomPrice(){
-        $price_list = array(50, 80, 100, 120);
-        return $price_list[array_rand($price_list)];
-    }
-
-    function randomPerf(){
-        $performance_list = array("60kW", "80kW", "90kW");
-        return $performance_list[array_rand($performance_list)];
-    }
-
-    function randomDescr(){
-        $description_list = array("new", "used", "mildly used");
-        return $description_list[array_rand($description_list)];
-    }
-
-    //fill the table with the generated data
-    $counter = 1;
-    while($counter <= 50){
-        $brand = randomBrand();
-        $type = randomType();
-        $price = randomPrice();
-        $perf = randomPerf();
-        $desc = randomDescr();
-        $sql = ("INSERT INTO tractors(brand, type,
-                price,performance,description)
-                VALUES(:brand,:type,:price,
-                :performance,:description)");
-
-        $instmt = $conn->prepare($sql);
-
-        $instmt->bindValue(':brand', $brand);
-        $instmt->bindValue(':type', $type);
-        $instmt->bindValue(':price', $price);
-        $instmt->bindValue(':performance', $perf);
-        $instmt->bindValue(':description', $desc);
-
-        $inserted = $instmt->execute();
-        if($instmt){
-            echo 'Row inserted!<br>';
-        }
-        $counter++;
-    }
-
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
+$stmt = $conn->query($mysql_query);
+if ($stmt) {
+    echo 'Table Created<br>';
+} else {
+    echo 'Table creation failed';
 }
+
+//Random genereated data
+
+function randomBrand(){
+    $brand_list = array("Toyota", "Lada", "Mazda", "Suzuki");
+    return $brand_list[array_rand($brand_list)];
+}
+
+function randomType(){
+    $type_list = array("HUN", "EN", "CH", "GER");
+    return $type_list[array_rand($type_list)];
+}
+
+function randomPrice(){
+    $price_list = array(50, 80, 100, 120);
+    return $price_list[array_rand($price_list)];
+}
+
+function randomPerf(){
+    $performance_list = array("60kW", "80kW", "90kW");
+    return $performance_list[array_rand($performance_list)];
+}
+
+function randomDescr(){
+    $description_list = array("new", "used", "mildly used");
+    return $description_list[array_rand($description_list)];
+}
+
+//fill the table with the generated data
+$counter = 1;
+while($counter <= 50){
+    $brand = randomBrand();
+    $type = randomType();
+    $price = randomPrice();
+    $perf = randomPerf();
+    $desc = randomDescr();
+    $sql = ("INSERT INTO tractors(brand, type,
+            price,performance,description)
+            VALUES(:brand,:type,:price,
+            :performance,:description)");
+
+    $instmt = $conn->prepare($sql);
+
+    $instmt->bindValue(':brand', $brand);
+    $instmt->bindValue(':type', $type);
+    $instmt->bindValue(':price', $price);
+    $instmt->bindValue(':performance', $perf);
+    $instmt->bindValue(':description', $desc);
+
+    $inserted = $instmt->execute();
+    $counter++;
+}
+echo $counter-1 .' row inserted!<br>';
+
 
 die();
